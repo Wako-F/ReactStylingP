32ba8c9818c20eb36f02fe92b5e1c7fd20a1165f
Wako-F <100088116+Wako-F@users.noreply.github.com>
2022-07-18 03:55:07 -0400

    Custom greeting that changes with time.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,30 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const time = new Date().getHours();
+var message = "Good Morning";
+const customStyle = {
+  color: ""
+};
+if (time > 12 && time < 18) {
+  customStyle.color = "green";
+  message = "Good Afternoon";
+} else if (time > 18 && time === 23) {
+  message = "Good Night";
+  customStyle.color = "blue";
+} else if (time > 0 && time < 12) {
+  customStyle.color = "red";
+}
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={customStyle}>
+      {" "}
+      {message}{" "}
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
